150372b8e5b73dc25b62066a2bc5bed6b307756f
Kostas Andreadis <kandread@umass.edu>
2023-03-22 11:46:47 -0400

    Check for missing data

--- swot.jl
+++ swot.jl
@@ -47,6 +47,9 @@ function read_swot_obs(ncfile::String, nids::Vector{Int})
         Hr = convert(Vector{Sad.FloatM}, reaches["wse"][:])
         Wr = convert(Vector{Sad.FloatM}, reaches["width"][:])
         Sr = convert(Vector{Sad.FloatM}, reaches["slope2"][:])
+        W[isnan.(H)] .= missing
+        S[isnan.(H)] .= missing
+        H[isnan.(H)] .= missing
         nid = nodes["node_id"][:]
         dmap = Dict(nid[k] => k for k=1:length(nid))
         i = [dmap[k] for k in nids]
