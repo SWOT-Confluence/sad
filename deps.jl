1a3a12516bb4cafca7a2d64b06d19b32eb7505a4
Nikki <17799906+nikki-t@users.noreply.github.com>
2024-08-15 11:07:23 -0400

    Download SoS file from S3 to locate temp storage

--- deps.jl
+++ deps.jl
@@ -1,8 +1,10 @@
 using Pkg
 
 Pkg.add(Pkg.PackageSpec(url="https://github.com/Hydro-Umass/Sad.jl"))
+Pkg.add("ArgParse")
 Pkg.add("PackageCompiler")
 Pkg.add("Distributions")
 Pkg.add("NCDatasets")
 Pkg.add("JSON")
+Pkg.add("PyCall")
 Pkg.precompile()
